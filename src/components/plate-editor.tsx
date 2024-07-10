f1c0770f8424cad453b092cb18cd019122f93fad
Cody Christ <cody@makerops.com>
2024-07-09 21:55:28 -0700

    Update PlateEditor component to import/export content

--- src/components/plate-editor.tsx
+++ src/components/plate-editor.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { cn } from '@udecode/cn';
 import { CommentsProvider } from '@udecode/plate-comments';
-import { Plate } from '@udecode/plate-common';
+import { createPlateEditor, getNode, Plate, useEditorState } from '@udecode/plate-common';
 import { ELEMENT_PARAGRAPH } from '@udecode/plate-paragraph';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
@@ -19,22 +19,71 @@ import { FixedToolbarButtons } from '@/components/plate-ui/fixed-toolbar-buttons
 import { FloatingToolbar } from '@/components/plate-ui/floating-toolbar';
 import { FloatingToolbarButtons } from '@/components/plate-ui/floating-toolbar-buttons';
 import { MentionCombobox } from '@/components/plate-ui/mention-combobox';
+import { serializeHtml } from '@udecode/plate-serializer-html';
+import { ErrorBoundary } from 'react-error-boundary';
 
-export default function PlateEditor() {
+interface ISerializedHtmlProps {
+  onError: (error: boolean) => void;
+}
+
+const temporaryEditor = createPlateEditor({ plugins });
+
+export const SerializedHtml = ({ onError }: ISerializedHtmlProps) => {
+  const editor = useEditorState();
+  let html;
+  try {
+    html = serializeHtml(editor, {
+      nodes: editor.children
+    });
+    onError(false);
+  } catch (err) {
+    html = '';
+    onError(true);
+  }
+  return <div>{html}</div>
+};
+
+const getNodesFromEditor = (editorValue: any[]) => {
+  return editorValue.map((item: any) => item.children?.length > 0 ? item.children[0] : null).filter(Boolean);
+};
+
+interface IPlateEditorProps {
+  exportTriggered?: number;
+  handleGenerateContent?: (contentHtml: string, contentValues: any[]) => void;
+  editorValue?: any[];
+  initialValue?: any;
+};
+
+export function EditorCallback({ exportTriggered, handleGenerateContent, editorValue }: IPlateEditorProps) {
+  const [lastExportTriggered, setLastExportTriggered] = useState(0);
+  const editor = useEditorState();
+  const contentHtml = serializeHtml(temporaryEditor, { nodes: lastExportTriggered !== exportTriggered ? getNodesFromEditor(editorValue || []) : [] });
+
+  useEffect(() => {
+    if (handleGenerateContent && exportTriggered !== lastExportTriggered) {
+      handleGenerateContent(contentHtml, editorValue || []);
+    }
+    if (exportTriggered) {
+      setLastExportTriggered(exportTriggered);
+    }
+  }, [exportTriggered]);
+
+  return null;
+}
+
+export default function PlateEditor(props: IPlateEditorProps) {
   const containerRef = useRef(null);
+  const [editorValue, setEditorValue] = useState<any[]>([]);
 
-  const initialValue = [
-    {
-      id: '1',
-      type: ELEMENT_PARAGRAPH,
-      children: [{ text: 'Hello, World!' }],
-    },
-  ];
+  const onPlateChange = (value: any[]) => {
+    setEditorValue(value);
+  };
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <CommentsProvider users={commentsUsers} myUserId={myUserId}>
-        <Plate plugins={plugins} initialValue={initialValue}>
+      {/* <CommentsProvider users={commentsUsers} myUserId={myUserId}> */}
+        <Plate plugins={plugins} initialValue={props.initialValue} onChange={onPlateChange}>
+          <EditorCallback editorValue={editorValue} {...props} />
           <div
             ref={containerRef}
             className={cn(
@@ -66,7 +115,7 @@ export default function PlateEditor() {
             <CursorOverlay containerRef={containerRef} />
           </div>
         </Plate>
-      </CommentsProvider>
+      {/* </CommentsProvider> */}
     </DndProvider>
   );
 }
