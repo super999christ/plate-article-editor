e2de1777748e4c9ee6cae63f539ebcb633b5529d
Cody Christ <cody@makerops.com>
2024-07-09 21:56:36 -0700

    Next.js pages with custom views

---
+++ src/app/article/[articleId]/page.tsx
@@ -0,0 +1,13 @@
+import { strapiClient } from "@/lib/strapi/strapi";
+import ArticleView from "@/views/ArticleView";
+
+interface IPageProps {
+  params: {
+    articleId: string;
+  };
+}
+
+export default async function ArticleDetailPage({ params }: IPageProps) {
+  const { data: article } = (await strapiClient.get(`/articles/${params.articleId}?populate[background][populate]=*&populate[title][populate]=*`)).data;
+  return <ArticleView article={article} />;
+}
---
+++ src/pages/api/update-article/index.ts
@@ -0,0 +1,21 @@
+import { strapiClient } from '@/lib/strapi/strapi';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'PUT') {
+    console.log('Unsupported method');
+    res.status(405).json({ message: 'Method not supported' });
+  }
+  try {
+    const { blogContent, articleId } = req.body;
+    await strapiClient.put(`/articles/${articleId}`, { data: { blogContent } });
+    return res.status(200).json({ message: 'Successfully saved the article content.' });
+  } catch (err: any) {
+    return res.status(500).json({
+      message: 'Something went wrong. Please try again some time later.',
+    });
+  }
+};
+
+export default handler;
